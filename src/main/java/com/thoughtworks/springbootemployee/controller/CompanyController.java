106b2478fd75eca0d7c2627a566e4f639c7b1ba9
Hans <741581879@qq.com>
2020-07-30 08:39:21 +0800

    [Company Employee][Hans Vicki] refactor: Call the service method from CompanyService class

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -2,6 +2,8 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedList;
@@ -12,23 +14,10 @@ import java.util.List;
 public class CompanyController {
     List<Company> companies = new LinkedList<>();
 
+    @Autowired
+    private CompanyService companyService;
+
     public CompanyController() {
-        this.companies.add(new Company(1, "OOCL"));
-        this.companies.add(new Company(2, "CargoSmart"));
-        this.companies.add(new Company(3, "Alibaba"));
-        this.companies.add(new Company(4, "Gree"));
-        this.companies.add(new Company(5, "Baidu"));
-        for (Company company : companies) {
-            List<Employee> employees = new LinkedList<>();
-            employees.add(new Employee(1, "Hans", 24, "male", 5000));
-            employees.add(new Employee(2, "Amy", 22,"female", 9000));
-            employees.add(new Employee(3, "Ray", 28,"male", 10000));
-            employees.add(new Employee(4, "Sky", 27,"female", 8000));
-            employees.add(new Employee(5, "Hovees", 25,"male", 7000));
-            employees.add(new Employee(6, "Mandy", 22,"male", 8888));
-            employees.add(new Employee(7, "Ace", 23,"male", 9000));
-            company.setEmployees(employees);
-        }
     }
 
     @GetMapping
@@ -37,63 +26,37 @@ public class CompanyController {
             @RequestParam(required = false, defaultValue = "0") int pageSize
     ) {
         if (page == 0) {
-            return companies;
+            return companyService.getAllCompanies();
         } else {
-            return getCompaniesByPage(page, pageSize);
+            return (List<Company>) companyService.getCompanyByPage(page, pageSize);
         }
-    }
-
-    private List<Company> getCompaniesByPage(int page, int pageSize) {
-        int begin;
-        int end;
-        int count = companies.size();
-
-        begin = (page - 1) * pageSize;
-        if (count - begin < pageSize) {
-            end = count - 1;
-        } else {
-            end = begin + pageSize;
-        }
-
-        return companies.subList(begin, end);
     }
 
     @PostMapping
     public Company createCompany(@RequestBody Company company) {
-        companies.add(company);
-        return companies.contains(company) ? company : null;
+        return companyService.createCompany(company);
     }
 
     @GetMapping("/{id}")
     public Company getCompany(@PathVariable int id) {
-        for(Company company : companies) {
-            if(company.getId() == id) {
-                return company;
-            }
-        }
-        return null;
+        return companyService.getCompanyById(id);
     }
 
     @GetMapping("/{id}/employees")
     public List<Employee> getAllEmployees(@PathVariable int id) {
-        Company company = companies.stream().filter(companyItem -> companyItem.getId() == id).findFirst().orElse(null);
-        return company == null ? null : company.getEmployees();
+        return companyService.getCompanyEmployeesById(id);
     }
 
     @PutMapping("/{id}")
-    public void updateCompany(@PathVariable int id, @RequestBody Company company) {
-        for (Company companyItem : companies) {
-            if (companyItem.getId() == id) {
-                companyItem.setEmployees(company.getEmployees());
-                companyItem.setId(company.getId());
-            }
-        }
+    public Company updateCompany(@PathVariable int id, @RequestBody Company company) {
+        Company updatedCompany = companyService.updateCompany(id, company);
+        return updatedCompany;
     }
 
     @DeleteMapping("/{id}")
 //    TODO return type
     public String deleteAllEmployees(@PathVariable int id) {
-        companies.stream().filter(companyItem -> companyItem.getId() == id).findFirst().ifPresent(company -> company.getEmployees().clear());
+        companyService.removeCompany(id);
         return "Delete Success";
     }
 }
